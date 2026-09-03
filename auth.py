ae11b72099c4aa20b1c5dedca0a0866137341e0a
ManishTanwar7 <mstanwar567@gmail.com>
2026-09-03 10:16:31 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -8,7 +8,8 @@ SECRET_KEY = "supersecretkey"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["sha256_crypt"], deprecated="auto")
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 fake_users_db = {
